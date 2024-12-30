12ee8afbb455a898e8ed13eb88d94269c24dd69f
warjohn <johnvoronina@gmail.com>
2024-12-30 16:53:36 +0700

    test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Menar/source/C++/socket
+# Build directory: C:/Users/Menar/source/C++/socket/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <openssl/ssl.h>
+
+
+int main(int, char**){
+    fmt::print("Hello World!\n");
+    system("pause");
+    return 0;
+}
